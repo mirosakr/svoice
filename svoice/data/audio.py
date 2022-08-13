0a55a69111c708dd3c18eddaf06e6c24a3785d03
mirosakr <88634831+mirosakr@users.noreply.github.com>
2022-08-13 12:12:00 +0200

    Update audio.py

--- svoice/data/audio.py
+++ svoice/data/audio.py
@@ -75,7 +75,7 @@ class Audioset:
                 num_frames = self.length
             #  out = th.Tensor(sf.read(str(file), start=offset, frames=num_frames)[0]).unsqueeze(0)
             out = torchaudio.load(str(file), offset=offset,
-                                  num_frames=num_frames)[-1]
+                                  num_frames=num_frames)[0]
             if self.augment:
                 out = self.augment(out.squeeze(0).numpy()).unsqueeze(0)
             if num_frames:
